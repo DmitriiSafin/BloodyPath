272bb2b549b0188b269fdf8e80ea5184c911a8f0
Dmitry Safin <developersafin@gmail.com>
2023-04-29 15:59:06 +0500

    added MenuScene

--- BloodyPath/GameViewController.swift
+++ BloodyPath/GameViewController.swift
@@ -16,7 +16,7 @@ class GameViewController: UIViewController {
         
         if let view = self.view as! SKView? {
             
-            let scene = GameScene(size: self.view.bounds.size)
+            let scene = MenuScene(size: self.view.bounds.size)
             // Set the scale mode to scale to fit the window
             scene.scaleMode = .aspectFill
             
